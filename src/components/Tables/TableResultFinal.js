79b228ac01537c716b2f7c2cc8a06f35b92423d2
esther-itam <you@example.com>
2021-11-12 16:18:42 +0100

    deployment

---
+++ src/components/Tables/TableResultFinal.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import LARAVEL_SERVER from '../Variable';
+import { useAppContext } from '../../Context';
+
+function TableResultFinal(){
+
+    const { state, dispatch } = useAppContext();
+   const [numberTeam, setNumberTeam] =useState([]);
+
+   useEffect(() => {
+        axios.get(`${LARAVEL_SERVER}/results`)
+        .then(res => {
+            console.log(res.data.data[0].length)
+             setNumberTeam(res.data.data[0].length);
+        },1000)
+        },[]); 
+console.log(state.results)
+        return(
+            <div>
+                {state.results.map((result)=>
+                <h1>L'équipe {result.teamName} remporte la partie avec {parseInt(result.userCount)} 🎁 retrouvés</h1>
+                )}
+               <table className="table bg-white table-bordered border-warning">
+                    <thead className="table-warning">
+                        <tr>
+                        <th scope="col"></th>
+                        <th scope="col">🥇</th>
+                        <th scope="col">🥈</th>
+                        {numberTeam>2 ? <th scope="col">🥉</th> : null}
+                        {numberTeam>3 ? <th scope="col">🏅</th> : null}
+                        </tr>
+                    </thead>
+                    <tbody>
+                     <tr>
+                        </tr>
+                        <tr>
+                        <th  className="table-warning" scope="row">Equipe</th>
+                       {state.results.map((result)=>
+                        <td>{result.teamName}</td>
+                        )}
+                        </tr>
+                        <tr>
+                        <th  className="table-warning" scope="row">Score</th>
+                       {state.results.map((result)=><td>{parseInt(result.userCount)} 🎁</td>)}
+                        </tr> 
+                    </tbody>
+                    </table>
+                    
+            </div>
+        )
+}
+
+export default TableResultFinal;
--- src/components/Team/RatingScreen.js
+++ src/components/Team/RatingScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavbarTeam from '../Navbar/NavbarTeam';
 import ButtonReplay from '../Buttons/ButtonReplay';
 import ButtonLeave from '../Buttons/ButtonLeave';
-import TableResult from '../Tables/TableResult';
+import TableResultFinal from '../Tables/TableResultFinal';
 import gifFinal from '../../pictures/final.gif';
 import axios from 'axios';
 import LARAVEL_SERVER from '../Variable';
@@ -29,12 +29,18 @@ class RatingScreen extends React.Component{
                 <div  className="containerResultFinal">
                 <div  className="containerResultTeamFinal">
                         <h1>Classement final des équipes</h1>
-                        <TableResult/>
+                        <TableResultFinal/>
                         <div className="containerLinkResultFinal">
                         <img className="gifFinal" src={gifFinal} alt="" width="400px"/>
                         <div className="containerLinkResultFinal2">
-                        <ButtonReplay/>  
+                        <span className="hover"> 
+                        <ButtonReplay/> 
+                        <p>Les compteurs seront remis à 0 et vous rejouerez avec les mêmes équipes</p>
+                        </span> 
+                        <span className="hover"> 
                         <ButtonLeave/> 
+                        <p>Les compteurs seront remis à 0 et les équipes seront supprimées</p>
+                        </span> 
                         </div>
 
                     </div>
